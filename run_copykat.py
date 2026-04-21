f6723e6da882121d99081225fee16f08201a9015
feiler98 <werner.feiler@ukmuenster.de>
2026-04-21 14:48:30 +0200

    CopyKat repo for benchmarking

--- run_copykat.py
+++ run_copykat.py
@@ -37,20 +37,34 @@ def get_hg_38_file_paths(target_path: Path) -> list:
     return [p for p in target_path.rglob("*__hg_38__RCM.csv") if p.is_file()]
 
 
+def get_hg_38_desc_paths(target_path: Path) -> dict:
+    return {p.stem: p for p in target_path.rglob("*__hg_38.txt")}
+
+
 def run_copykat(path_target: Path,
                 path_out_data: Path,
-                norm_cell_names_tag = "",
                 n_cores: int = 10,
                 n_genes_chr: int = 5,
                 window_size: int = 25,
                 low_dr: float = 0.05,
                 up_dr: float = 0.1,
-                ks_cut: float = 0.1):
+                ks_cut: float = 0.1,
+                cell_pre_label: bool = False):
     list_paths_target_csvs = get_hg_38_file_paths(path_target)
+    if cell_pre_label:
+        dict_paths_target_txts = get_hg_38_desc_paths(path_target)
+        list_paths_target_csvs = [p for p in list_paths_target_csvs if p.stem.split("__RCM")[0] in dict_paths_target_txts.keys()]
     for p in list_paths_target_csvs:
-        name_tag = f"{p.stem}__nt{norm_cell_names_tag}c{n_cores}ngc{n_genes_chr}ws{window_size}ld{low_dr}ud{up_dr}ks{ks_cut}_"
-        path_out_target = path_out_data / f"out__{name_tag}_copykat"
+        name_tag = f"{p.stem}__n_cores,{n_cores};n_genes_chr,{n_genes_chr};window_size,{window_size};low_dr,{low_dr};up_dr,{up_dr};ks_cut,{ks_cut};cell_pre_label,{cell_pre_label}__copykat_"
+        path_out_target = path_out_data / f"out__{name_tag}"
         path_out_target.mkdir(parents=True, exist_ok=True)
+
+        norm_cell_vector = ""
+        if cell_pre_label:
+            path_txt = dict_paths_target_txts[p.stem.split("__RCM")[0]]   # normal cells split by \n
+            with open(path_txt, "r") as f:
+                list_norm_cells = list(map(lambda x: x.replace("\n", ""), f.readlines()))
+                norm_cell_vector = robjects.vectors.StrVector(list_norm_cells)
 
         ################################################################################################################
         # run copykat R-script
@@ -63,10 +77,10 @@ def run_copykat(path_target: Path,
                         ks_cut,
                         low_dr,
                         up_dr,
-                        norm_cell_names_tag):
+                        norm_cell_vector):
             r = robjects.r
             r.source("c_copykatR.R")
-            r.r_run_copykat(str(p), name_tag, n_cores, n_genes_chr, window_size, ks_cut, low_dr, up_dr, norm_cell_names_tag)
+            r.r_run_copykat(str(p), name_tag, n_cores, n_genes_chr, window_size, ks_cut, low_dr, up_dr, norm_cell_vector)
 
         run_rscript(p,
                     name_tag,
@@ -76,7 +90,7 @@ def run_copykat(path_target: Path,
                     ks_cut,
                     low_dr,
                     up_dr,
-                    norm_cell_names_tag)
+                    norm_cell_vector)
         ################################################################################################################
 
         # reformat *__copykat_CNA_raw_results_gene_by_cell.txt to final GBC-format
@@ -97,15 +111,16 @@ if __name__ == "__main__":
     # matrix of possible copykat hyperparameter kwargs
     kwargs_gridsearch = {
         "n_cores": [5, 10, 20],
-        "n_genes_chr": [1, 10, 100],
+        "n_genes_chr": [1, 5, 10, 100],
         "window_size": [10, 25, 50, 100, 200, 500],
         "low_dr": [0, 0.05, 0.1, 0.2],
         "up_dr": [0, 0.1, 0.2],
-        "ks_cut": [0, 0.1, 0.2]
+        "ks_cut": [0, 0.1, 0.2],
+        "cell_pre_label": [True, False]
     }
 
     path_in, path_out = val_build_project()
-    run_copykat(path_in, path_out, n_cores=1)  # standard parameters; one core
+    run_copykat(path_in, path_out, n_cores=10, cell_pre_label=True)  # standard parameters; one core
     list_kwargs = grid_by_dict(kwargs_gridsearch)
     for kwarg_opt in list_kwargs:
         print(f"CopyKat running with hyperparameters: {kwarg_opt}")
